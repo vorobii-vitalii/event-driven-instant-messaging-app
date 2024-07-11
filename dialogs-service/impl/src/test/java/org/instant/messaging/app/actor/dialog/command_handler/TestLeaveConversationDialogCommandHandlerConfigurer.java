180fd5606ca29b3fdf288eca3ffc1d179b150f82
Vitalii Vorobii <vitalii.vorobii@intellias.com>
2024-07-11 11:31:30 +0100

    more tests for leave conversation command handler

--- dialogs-service/impl/src/test/java/org/instant/messaging/app/actor/dialog/command_handler/TestLeaveConversationDialogCommandHandlerConfigurer.java
+++ dialogs-service/impl/src/test/java/org/instant/messaging/app/actor/dialog/command_handler/TestLeaveConversationDialogCommandHandlerConfigurer.java
@@ -10,7 +10,9 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.instant.messaging.app.actor.dialog.command.LeaveConversationCommand;
+import org.instant.messaging.app.actor.dialog.event.DialogClosedEvent;
 import org.instant.messaging.app.actor.dialog.event.DialogEvent;
+import org.instant.messaging.app.actor.dialog.event.NewLeaderChosenEvent;
 import org.instant.messaging.app.actor.dialog.event.ParticipantLeftEvent;
 import org.instant.messaging.app.actor.dialog.state.ActiveDialogState;
 import org.instant.messaging.app.actor.dialog.state.DialogState;
@@ -53,6 +55,69 @@ class TestLeaveConversationDialogCommandHandlerConfigurer extends BaseDialogComm
 								))
 								.thenReply(replyTo, v -> StatusReply.ack()));
 	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	void givenParticipantIsStillPresentInDialogAndHeIsLeaderButThereAreOtherParticipantsInChat() {
+		UUID participant1 = UUID.randomUUID();
+		UUID participant2 = UUID.randomUUID();
+		ActorRef<StatusReply<Done>> replyTo = mock(ActorRef.class);
+		final Instant timestamp = Instant.now();
+
+		when(actorContext.getLog()).thenReturn(LOGGER);
+
+		ReplyEffect<DialogEvent, DialogState> replyEffect = createCommandHandler().apply(
+				ActiveDialogState.builder()
+						.leader(participant1)
+						.participants(Set.of(participant1, participant2))
+						.build(),
+				LeaveConversationCommand.builder()
+						.requester(participant1)
+						.timestamp(timestamp)
+						.replyTo(replyTo)
+						.build());
+		assertThat(replyEffect)
+				.usingRecursiveComparison()
+				.isEqualTo(
+						new EffectFactories<DialogEvent, DialogState>()
+								.persist(List.of(
+										new ParticipantLeftEvent(participant1, timestamp),
+										new NewLeaderChosenEvent(participant2, timestamp)
+								))
+								.thenReply(replyTo, v -> StatusReply.ack()));
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	void givenParticipantIsStillPresentInDialogAndHeIsLeaderAndThereAreNoOtherParticipantsInChat() {
+		UUID participant1 = UUID.randomUUID();
+		UUID participant2 = UUID.randomUUID();
+		ActorRef<StatusReply<Done>> replyTo = mock(ActorRef.class);
+		final Instant timestamp = Instant.now();
+
+		when(actorContext.getLog()).thenReturn(LOGGER);
+
+		ReplyEffect<DialogEvent, DialogState> replyEffect = createCommandHandler().apply(
+				ActiveDialogState.builder()
+						.leader(participant1)
+						.participants(Set.of(participant1))
+						.build(),
+				LeaveConversationCommand.builder()
+						.requester(participant1)
+						.timestamp(timestamp)
+						.replyTo(replyTo)
+						.build());
+		assertThat(replyEffect)
+				.usingRecursiveComparison()
+				.isEqualTo(
+						new EffectFactories<DialogEvent, DialogState>()
+								.persist(List.of(
+										new ParticipantLeftEvent(participant1, timestamp),
+										new DialogClosedEvent(timestamp)
+								))
+								.thenReply(replyTo, v -> StatusReply.ack()));
+	}
+
 
 	@SuppressWarnings("unchecked")
 	@Test
